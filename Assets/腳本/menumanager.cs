46aed6ae392e6a8a01270b464f635babf2b15c78
tt0302 <55473218+tt0302@users.noreply.github.com>
2020-11-28 15:10:36 +0800

    web
    web

---
+++ Assets/腳本/menumanager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        print("開始遊戲");  
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        print("離開遊戲");
+    }
+}
